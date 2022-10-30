855b38ea6149af4925eb36a7387d943bb73be30e
Ezechiel Iti <papaezechiel@sympatico.ca>
2022-10-29 20:03:23 -0400

    getstarted done

--- siloe_churh_app/src/components/GetStarted.jsx
+++ siloe_churh_app/src/components/GetStarted.jsx
@@ -6,10 +6,10 @@ const GetStarted = () => (
     className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
   >
     <div
-      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[-100%] rounded-full`}
+      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
     >
       <div className={`${styles.flexStart} flex-row`}>
-        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
           <span className="text-gradient">Get</span>
           <img
             src={arrowUp}
@@ -18,6 +18,10 @@ const GetStarted = () => (
           />
         </p>
       </div>
+
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Started</span>
+        </p>
     </div>
   </div>
 );
